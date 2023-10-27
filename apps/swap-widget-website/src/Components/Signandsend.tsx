c26f1f702b0752ee09677372a0576cfbbe81febb
Kamal Nayan <geniusamansingh@gmail.com>
2023-10-27 04:20:59 +0000

    signing and sending working using perawallet working

--- apps/swap-widget-website/src/Components/Signandsend.tsx
+++ apps/swap-widget-website/src/Components/Signandsend.tsx
@@ -1,28 +1,45 @@
 "use client";
 import { PeraWalletConnect } from "@perawallet/connect";
 import { useEffect, useState } from "react";
+import { decodeUnsignedTransaction } from "algosdk";
+import { MainnetAlgodClient , sender , TestnetAlgodClient} from '../utils/config';
 
 const peraWallet = new PeraWalletConnect();
 
 export default function Signandsend() {
   const [accountAddress, setAccountAddress] = useState("");
+  console.log(accountAddress)
   const isConnectedToPeraWallet = !!accountAddress;
+  // encoded txn returned from api
   const optInTxn = [
-    "i6RhYW10ZqRhcmN2xCAqwST03PAq66gItM+bhmBgqIKWeldEAmbcMmVybADfRqNmZWXNA+iiZnbOAgfq9KNnZW6sdGVzdG5ldC12MS4womdoxCBIY7UYpLPITsgQ8i1PEIHLD3HwWaesIN7GL39w5Qk6IqNncnDEIEfLSatdE8vfUblP15S6QYLUcNLeLwB6PRiSt47A/SuComx2zgIH7tyjc25kxCBCHpf3xMbBHWeerszijgsGOapCsnqxUfPN/0dYyKH5Z6R0eXBlpWF4ZmVypHhhaWTOBARhFg==",
-    "jaRhcGFhlsQE8GcUyMQIAAAAAAAAAAHECAAAAAAEBGEWxAgAAAAAAAAnEMQIAAAAAAAAAADEowABAAAAAAAAAAEAAAAAAAQEYRYAAAAABARjTgAAAAAT7ufv+OdM4xoxu/u2qAjMjRDLb2pO7XdqKUEqK5m5O/gZI1gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACkYXBhc5LOBARhFs4EBGNOpGFwZmGRzhPu5++kYXBpZM4Zqjtlo2ZlZc0q+KJmds4CB+r0o2dlbqx0ZXN0bmV0LXYxLjCiZ2jEIEhjtRiks8hOyBDyLU8QgcsPcfBZp6wg3sYvf3DlCToio2dycMQgR8tJq10Ty99RuU/XlLpBgtRw0t4vAHo9GJK3jsD9K4KibHbOAgfu3KRub3RlxQF7eyJub3RlIjp7IjY3Mzk1ODYyIjowLjc2NzI1NDM2MzMwNzI4NDksIjY3Mzk2NDMwIjowLjc2NTk4NTU3MTczNjY5MzUsIm1vZGUiOiJGSVhFRF9PVVRQVVQiLCJpbnB1dEFzc2V0SWQiOjY3Mzk1ODYyLCJvdXRwdXRBc3NldElkIjo2NzM5NjQzMCwiYW1vdW50IjoiMSIsInF1b3RlQW1vdW50IjoiMTAyIiwicHJpY2VJbXBhY3QiOjEwMS4xNjg5NTQ1NDAzNjY3NCwicGF0aHMiOlt7InBlcmNlbnRCcHMiOiIxMDAwMCIsInN3YXBzIjpbMzM0NDI0MDQ3XX1dLCJmZWVCcHMiOiIxMCIsIm91dHB1dEFtb3VudEZlZSI6IjAiLCJzbGlwcGFnZUJwcyI6IjEifSwibm9uY2UiOiIwNDY5NjU0YzM0NGJlYjY0MjUzYzA4OGYyMzQ4MWMzYiIsImNoZWNrc3VtIjoiOWMwNmMxNzgifaNzbmTEIEIel/fExsEdZ56uzOKOCwY5qkKyerFR883/R1jIoflnpHR5cGWkYXBwbA==",
-    "i6RhcGFhlsQE6cgNPcQIAAAAAAQEY07ECAAAAAAAAAABxAgAAAAAAAAACsQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADEowABAAAAAAAAAAEAAAAAAAQEY04AAAAABARhFgAAAAAT7ufv+OdM4xoxu/u2qAjMjRDLb2pO7XdqKUEqK5m5O/gZI1gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACkYXBhc5HOBARjTqRhcGF0kcQgj97vIQa0eptlq/DIxzezkbVbs27vLAOh1MI0oLPgZcWkYXBpZM4ZqjtlomZ2zgIH6vSjZ2VurHRlc3RuZXQtdjEuMKJnaMQgSGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiKjZ3JwxCBHy0mrXRPL31G5T9eUukGC1HDS3i8Aej0YkreOwP0rgqJsds4CB+7co3NuZMQgQh6X98TGwR1nnq7M4o4LBjmqQrJ6sVHzzf9HWMih+WekdHlwZaRhcHBs"
+    "iqNhbXTOAHX1b6NmZWXNA+iiZnbOAgggoaNnZW6sdGVzdG5ldC12MS4womdoxCBIY7UYpLPITsgQ8i1PEIHLD3HwWaesIN7GL39w5Qk6IqNncnDEIGK+0ImoJETwvq9PHCZZA7X7NTP5eDyuiIK23RjPmBUtomx2zgIIJImjcmN2xCAqwST03PAq66gItM+bhmBgqIKWeldEAmbcMmVybADfRqNzbmTEIEIel/fExsEdZ56uzOKOCwY5qkKyerFR883/R1jIoflnpHR5cGWjcGF5",
+    "jaRhcGFhlsQE8GcUyMQIAAAAAAAAAAHECAAAAAAAAAAAxAgAAAAAAAAnEMQIAAAAAAAAAADEowABAAAAAAAAAAEAAAAAAAAAAAAAAAAABARhFgAAAAAT7yP5kNF8XoAuS7VYiSV8/xIP3L+InyY1+ft6gsNmvBKadlsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACkYXBhc5IAzgQEYRakYXBmYZHOE+8j+aRhcGlkzhmqO2WjZmVlzSr4omZ2zgIIIKGjZ2VurHRlc3RuZXQtdjEuMKJnaMQgSGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiKjZ3JwxCBivtCJqCRE8L6vTxwmWQO1+zUz+Xg8roiCtt0Yz5gVLaJsds4CCCSJpG5vdGXFAXF7Im5vdGUiOnsiMCI6MC4xMDAwNSwiNjczOTU4NjIiOjAuNzYzOTUyMTMzODY1MTA0MiwibW9kZSI6IkZJWEVEX09VVFBVVCIsImlucHV0QXNzZXRJZCI6MCwib3V0cHV0QXNzZXRJZCI6NjczOTU4NjIsImFtb3VudCI6IjEwMDAwMDAiLCJxdW90ZUFtb3VudCI6Ijc3Mjk3NzEiLCJwcmljZUltcGFjdCI6MC4wMTIzMTk0Mjk4MDE1NTA0NDcsInBhdGhzIjpbeyJwZXJjZW50QnBzIjoiMTAwMDAiLCJzd2FwcyI6WzMzNDQzOTQxN119XSwiZmVlQnBzIjoiMTAiLCJvdXRwdXRBbW91bnRGZWUiOiIxMDAxIiwic2xpcHBhZ2VCcHMiOiIxIn0sIm5vbmNlIjoiNDY5ZmMwYjdlNTdkZGI0NTE3YjYwYzViN2FjMGU1NzQiLCJjaGVja3N1bSI6ImNiYjU0NmJmIn2jc25kxCBCHpf3xMbBHWeerszijgsGOapCsnqxUfPN/0dYyKH5Z6R0eXBlpGFwcGw=",
+    "i6RhcGFhlsQE6cgNPcQIAAAAAAQEYRbECAAAAAAAD0JAxAgAAAAAAAAACsQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADEowABAAAAAAAAAAEAAAAAAAQEYRYAAAAAAAAAAAAAAAAT7yP5kNF8XoAuS7VYiSV8/xIP3L+InyY1+ft6gsNmvBKadlsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACkYXBhc5HOBARhFqRhcGF0kcQgj97vIQa0eptlq/DIxzezkbVbs27vLAOh1MI0oLPgZcWkYXBpZM4ZqjtlomZ2zgIIIKGjZ2VurHRlc3RuZXQtdjEuMKJnaMQgSGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiKjZ3JwxCBivtCJqCRE8L6vTxwmWQO1+zUz+Xg8roiCtt0Yz5gVLaJsds4CCCSJo3NuZMQgQh6X98TGwR1nnq7M4o4LBjmqQrJ6sVHzzf9HWMih+WekdHlwZaRhcHBs"
   ]
-  const singleTxnGroups = [{txn: optInTxn, signers: ["IIPJP56EY3AR2Z46V3GOFDQLAY42UQVSPKYVD46N75DVRSFB7FT6244NZU"]}];
-
-  async function lfg(){
+  // const singleTxnGroups = [{txn: optInTxn, signers: ["IIPJP56EY3AR2Z46V3GOFDQLAY42UQVSPKYVD46N75DVRSFB7FT6244NZU"]}];
+  
+  const algod = TestnetAlgodClient;
+  async function SignusingPerraWallet(){
     try {
         // @ts-ignore
-        const signedTxn = await peraWallet.signTransaction([singleTxnGroups]);
-        console.log(signedTxn,"signed txn");
+        const unsignedTxns = optInTxn.map(txn => decodeUnsignedTransaction(Buffer.from(txn, "base64")));
+        console.log(unsignedTxns,"unsigned");
+        const multipleTxnGroups = [
+          {txn: unsignedTxns[0], signers: [accountAddress]},
+          {txn: unsignedTxns[1], signers: [accountAddress]},
+          {txn: unsignedTxns[2], signers: [accountAddress]}
+        ];
+        const signedTxns = await peraWallet.signTransaction([multipleTxnGroups], accountAddress);
+        // const signedTxns = unsignedTxns.map(txn => txn.signTxn(user.sk));
+        console.log("signed txn",signedTxns);
+        // submit
+        const sendraw = await algod.sendRawTransaction(signedTxns).do();
+        console.log(sendraw,"signed txn");
       } catch (error) {
         console.log("Couldn't sign Opt-in txns", error);
       }
-  }
+  };
+
   useEffect(() => {
     // Reconnect to the session when the component is mounted
     if (typeof window !== 'undefined') {
@@ -52,8 +69,9 @@ export default function Signandsend() {
     >
       {isConnectedToPeraWallet ? "Disconnect" : "Connect to Pera Wallet"}
     </button>
-    <button
-    onClick={lfg}> Swap</button>
+    <button onClick={SignusingPerraWallet} className="text-green-400 mx-4">
+      Swap
+    </button>
     </>
   );
 
